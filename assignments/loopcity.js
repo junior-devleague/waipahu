62b761690c93ddeaa8caf91cb71e85737eefb09b
jmt75200 <joelle@pcgamerzhawaii.com>
2016-11-28 16:45:17 -1000

    adding loop js assignment

---
+++ assignments/loopcity.js
@@ -0,0 +1,52 @@
+/*
+  Loops on Loops on Loops
+  Nov. 28, 2016
+  Joelle Torneros & Jon Tan
+*/
+
+// Mommy I can count to 100
+/*
+  Create a variable named Count
+  Create a for loop that counts from 0 to 100
+  Console log "Yan can count to " and the number.
+*/
+
+
+
+
+// Noah's Arc
+/*
+  Create a variable named animalCount
+  Create a for loop that loops a maximum of 5 times
+  Console Log "We have x many animals"
+*/
+
+
+
+
+// Hip Hop Hurray
+/*
+  Create a variable partyAnimals that has an array of 5 string values.
+  Loop through the partyAnimals and console log a unique sentence of your choice.
+*/
+
+
+
+
+
+// Merlin's Battle
+/*
+  Make sure Merlin has equiped the correct weapon to defeat the dragon.
+  Create an variable weaponChest that has an array of Mace, Sorcerer's Stone and Excalibur.
+  Create an object hero and set the following values:
+    wizard named Merlin
+    weapon from the weapon's chest
+    power set to a number from 1 - 50
+
+  Create a function battleReady that takes in one parameter, choice.
+  If Merlin's choice is not the Sorcerer's Stone, equip it
+   then console log "Our " + hero.wizard + " is now battle ready"
+  If his choice in weapon is the Sorcerer's Stone then console log hero.wizard + " has slained the dragon!"
+
+*/
+
